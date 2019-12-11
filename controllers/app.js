bb89771a4e07d71afafc8a16864a33c3ff3a3057
Sheikh hamza <sheikhhamza012@gmail.com>
2019-12-11 16:14:20 +0500

    apis complete with postman export v2

--- controllers/app.js
+++ controllers/app.js
@@ -2,7 +2,7 @@ const User= require('../models/User')
 module.exports={
     index : (req,res)=>{
         console.log("get")
-        res.send(req.username)
+        res.send("Live")
     },
     
 }
--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,14 @@ const storage = multer.diskStorage({
   })
 const upload = multer({ storage:storage })
 module.exports = (app)=>{
-    app.get('/', Authenticate.verifyToken, PostCtrl.index)
+    app.get('/', AppCtrl.index)
     app.post('/signup', UserCtrl.signup)
     app.post('/login',UserCtrl.login)
     app.post('/user/update/password',Authenticate.verifyToken,UserCtrl.updatePassword)
     app.get('/user/follow/:username',Authenticate.verifyToken,UserCtrl.follow)
     app.get('/user/unfollow/:username',Authenticate.verifyToken,UserCtrl.unfollow)
 
-    // app.get('/posts',Authenticate.verifyToken,PostCtrl.index)
+    app.get('/posts',Authenticate.verifyToken,PostCtrl.index)
     app.post('/posts/new',Authenticate.verifyToken, upload.single('image'),uploadfile,PostCtrl.new)
     app.get('/posts/:username',Authenticate.verifyToken,PostCtrl.userPosts)
     app.delete('/posts/:id',Authenticate.verifyToken,PostCtrl.deletePost)
